1581bc8179c72ee05878ad893ac6f6649acedc7b
Iain Forbes <i.forbes77@gmail.com>
2020-11-24 22:04:41 +0000

    album update

--- models/album.py
+++ models/album.py
@@ -30,7 +30,7 @@ def select(id):
     values = [id]
     result = run_sql(sql, values)[0]
     if result is not None:
-        artist = Artist(result['name'], result['id'])
+        artist = Artist(result['name'],result['id'])
     return artist
 
 # UPDATE
